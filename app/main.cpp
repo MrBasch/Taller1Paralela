dd3e4fe8dff933255c7bae62b1d1892045c98247
MrBasch <mikhail.baschsmanna@utem.cl>
2020-09-19 03:27:14 -0300

    Ahora detecta los elementos del polinomio

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include "funciones.h"
+#include <type_traits>
+#include <stdio.h>
+#include <cstring>
+
 /**
  * Taller computacional
  * @param argc cantidad de argumentos
@@ -10,12 +14,48 @@
 int main(int argc, char* argv[]) {
     std::cout << std ::endl;
     char* algoritmo = argv[1];
-    int largo= sizeof(algoritmo)/sizeof(algoritmo[0]) - 2 ;
-    //std::cout << " el largo es " << largo << std::endl;
-    for (int i=0; i<largo ; i++){
-        std::cout << " elemento en la posición" << i << " es " << algoritmo[i] << std::endl;
-        //if(algoritmo[i])    
+    int largo= (unsigned)strlen(argv[1]);
+    std::cout << std ::endl;
+    char x1;
+    char x2;
+    char x3;
+    for (int i=0; i< largo ; i++){
+        std::cout << " elemento en la posición " << i << " es " << algoritmo[i] << std::endl;
+        if(algoritmo[i] == 'x' and largo>i+1){
+            if(algoritmo[i+1] == '*' and algoritmo[i+3] == '3'){
+                x3 = algoritmo[i-1];
+                if(algoritmo[i-1] == ' '){
+                    x3 ='1';
+                }   
+            }
+             if(algoritmo[i+1] == '*' and algoritmo[i+3] == '2'){
+                x2 = algoritmo[i-1];
+                if(algoritmo[i-1] == ' '){
+                    x2 ='1';
+                }     
+            }
+             if(algoritmo[i+1] == '*' and algoritmo[i+3] == '1'){
+                x1 = algoritmo[i-1];  
+                if(algoritmo[i-1] == ' '){
+                    x1 ='1';
+                }  
+            }
+            
+        }
+        if(algoritmo[i] == 'x' and largo==i+1){
+            x1 = algoritmo[i-1];
+            if(algoritmo[i-1] == ' '){
+                    x1 ='1';
+                }    
+        }
+
     }
+    std::cout <<"el valor de x1 es "<<x1;
+    std::cout << std ::endl;
+    std::cout <<"el valor de x2 es "<<x2;
+    std::cout << std ::endl;
+    std::cout <<"el valor de x3 es "<<x3;
+    std::cout << std ::endl;
     participantes();
     std::cout << std ::endl;
     return 0; 
