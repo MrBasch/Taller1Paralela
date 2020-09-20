9b2744197f0b8244c106a5001bc603985a076974
MrBasch <mikhail.baschsmanna@utem.cl>
2020-09-20 20:02:37 -0300

    correccion nombre integrantes

---
+++ app/funciones.cpp
@@ -0,0 +1,13 @@
+#include "funciones.h"
+#include <iostream>
+void imprimir(char* texto){
+    std::cout<< texto << std::endl;
+}
+
+void participantes(){
+    std::cout << std::endl << "===Taller I ===" << std::endl;
+    std::cout << std::endl << "Mikhail Baschsmann";
+    std::cout << std::endl << "Ignacia Loyola" << std::endl;
+}
+
+
---
+++ app/funciones.h
@@ -0,0 +1,19 @@
+#ifndef FUNCIONES.H
+#define FUNCIONES.H
+#include <iostream>
+
+/**
+ * Imprime una cadena de texto por la salida estandar.
+ * 
+ * @param texto Mensaje a imprimir en patanlla
+ */
+void imprimir(char* texto);
+
+/**
+ * Funcion que muestra los participantes del grupo
+ */
+void participantes();
+
+
+
+#endif /* FUNCIONES.H */
